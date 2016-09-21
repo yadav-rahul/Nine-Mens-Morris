b68d5dbade3bee3c268b0e9e80b8d400a4aacf4f
Rahul Yadav <rahuly247@gmail.com>
2016-09-22 02:46:57 +0530

    Added js and css

---
+++ script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Rahul on 9/22/2016.
+ */
+function startGame() {
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
