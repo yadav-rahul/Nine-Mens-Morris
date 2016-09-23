23fc5f94217bf5adfeab0945bd77af0bb83f1416
yadav-rahul <rahuly247@gmail.com>
2016-09-23 22:59:26 +0530

    Completed basic game loop

--- script.js
+++ script.js
@@ -2,6 +2,14 @@
  * Created by Rahul on 9/22/2016.
  */
 
+var isActiveRed = false;
+var isActiveGreen = false;
+var blockWidth = 16;
+var strokeWidth = 2;
+var lastX = 0;
+var lastY = 0;
+var lastCenterX = 0;
+var lastCenterY = 0;
 var numberOfTurns = 0;
 var totalPositions = 24;
 var positionArray = new Array(7);
@@ -11,7 +19,7 @@ var context = canvas.getContext("2d");
 
 function initializeGame() {
     initializeArray();
-  //  alert("Player 1 turns first followed by Player 2");
+    //  alert("Player 1 turns first followed by Player 2");
     makeMove();
 }
 
@@ -37,212 +45,274 @@ function initializeArray() {
 
 }
 
-function update() {
-
-}
 function makeMove(X, Y) {
     var yCenter;
     var xCenter;
-    //Here X, Y are the index of array at which touch event occurred
-    if (positionArray[X][Y] == 0 && numberOfTurns <=18) {
-        switch (X) {
-            case 0: {
-                switch (Y) {
-                    case 0: {
-                        yCenter = 25;
-                        xCenter = 25;
-                        break;
-                    }
-                    case 3: {
-                        yCenter = 275;
-                        xCenter = 25;
-                        break;
-                    }
-                    case 6: {
-                        yCenter = 525;
-                        xCenter = 25;
-                        break;
-                    }
+
+    switch (X) {
+        case 0: {
+            switch (Y) {
+                case 0: {
+                    yCenter = 25;
+                    xCenter = 25;
+                    break;
                 }
-                break;
-            }
-            case 1: {
-                switch (Y) {
-                    case 1: {
-                        yCenter = 115;
-                        xCenter = 115;
-                        break;
-                    }
-                    case 3: {
-                        yCenter = 275;
-                        xCenter = 115;
-                        break;
-                    }
-                    case 5: {
-                        yCenter = 435;
-                        xCenter = 115;
-                        break;
-                    }
+                case 3: {
+                    yCenter = 275;
+                    xCenter = 25;
+                    break;
                 }
-                break;
-            }
-            case 2: {
-                switch (Y) {
-                    case 2: {
-                        yCenter = 195;
-                        xCenter = 195;
-                        break;
-                    }
-                    case 3: {
-                        yCenter = 275;
-                        xCenter = 195;
-                        break;
-                    }
-                    case 4: {
-                        yCenter = 355;
-                        xCenter = 195;
-                        break;
-                    }
+                case 6: {
+                    yCenter = 525;
+                    xCenter = 25;
+                    break;
                 }
-                break;
             }
-            case 3: {
-                switch (Y) {
-                    case 0: {
-                        yCenter = 25;
-                        xCenter = 275;
-                        break;
-                    }
-                    case 1: {
-                        yCenter = 115;
-                        xCenter = 275;
-                        break;
-                    }
-                    case 2: {
-                        yCenter = 195;
-                        xCenter = 275;
-                        break;
-                    }
-                    case 4: {
-                        yCenter = 355;
-                        xCenter = 275;
-                        break;
-                    }
-                    case 5: {
-                        yCenter = 435;
-                        xCenter = 275;
-                        break;
-                    }
-                    case 6: {
-                        yCenter = 525;
-                        xCenter = 275;
-                        break;
-                    }
+            break;
+        }
+        case 1: {
+            switch (Y) {
+                case 1: {
+                    yCenter = 115;
+                    xCenter = 115;
+                    break;
                 }
-                break;
-            }
-            case 4: {
-                switch (Y) {
-                    case 2: {
-                        yCenter = 195;
-                        xCenter = 355;
-                        break;
-                    }
-                    case 3: {
-                        yCenter = 275;
-                        xCenter = 355;
-                        break;
-                    }
-                    case 4: {
-                        yCenter = 355;
-                        xCenter = 355;
-                        break;
-                    }
+                case 3: {
+                    yCenter = 275;
+                    xCenter = 115;
+                    break;
                 }
-                break;
-            }
-            case 5: {
-                switch (Y) {
-                    case 1: {
-                        yCenter = 115;
-                        xCenter = 435;
-                        break;
-                    }
-                    case 3: {
-                        yCenter = 275;
-                        xCenter = 435;
-                        break;
-                    }
-                    case 5: {
-                        yCenter = 435;
-                        xCenter = 435;
-                        break;
-                    }
+                case 5: {
+                    yCenter = 435;
+                    xCenter = 115;
+                    break;
                 }
-                break;
             }
-            case 6: {
-                switch (Y) {
-                    case 0: {
-                        yCenter = 25;
-                        xCenter = 525;
-                        break;
-                    }
-                    case 3: {
-                        yCenter = 275;
-                        xCenter = 525;
-                        break;
-                    }
-                    case 6: {
-                        yCenter = 525;
-                        xCenter = 525;
-                        break;
-                    }
+            break;
+        }
+        case 2: {
+            switch (Y) {
+                case 2: {
+                    yCenter = 195;
+                    xCenter = 195;
+                    break;
                 }
-                break;
+                case 3: {
+                    yCenter = 275;
+                    xCenter = 195;
+                    break;
+                }
+                case 4: {
+                    yCenter = 355;
+                    xCenter = 195;
+                    break;
+                }
             }
+            break;
+        }
+        case 3: {
+            switch (Y) {
+                case 0: {
+                    yCenter = 25;
+                    xCenter = 275;
+                    break;
+                }
+                case 1: {
+                    yCenter = 115;
+                    xCenter = 275;
+                    break;
+                }
+                case 2: {
+                    yCenter = 195;
+                    xCenter = 275;
+                    break;
+                }
+                case 4: {
+                    yCenter = 355;
+                    xCenter = 275;
+                    break;
+                }
+                case 5: {
+                    yCenter = 435;
+                    xCenter = 275;
+                    break;
+                }
+                case 6: {
+                    yCenter = 525;
+                    xCenter = 275;
+                    break;
+                }
+            }
+            break;
+        }
+        case 4: {
+            switch (Y) {
+                case 2: {
+                    yCenter = 195;
+                    xCenter = 355;
+                    break;
+                }
+                case 3: {
+                    yCenter = 275;
+                    xCenter = 355;
+                    break;
+                }
+                case 4: {
+                    yCenter = 355;
+                    xCenter = 355;
+                    break;
+                }
+            }
+            break;
+        }
+        case 5: {
+            switch (Y) {
+                case 1: {
+                    yCenter = 115;
+                    xCenter = 435;
+                    break;
+                }
+                case 3: {
+                    yCenter = 275;
+                    xCenter = 435;
+                    break;
+                }
+                case 5: {
+                    yCenter = 435;
+                    xCenter = 435;
+                    break;
+                }
+            }
+            break;
+        }
+        case 6: {
+            switch (Y) {
+                case 0: {
+                    yCenter = 25;
+                    xCenter = 525;
+                    break;
+                }
+                case 3: {
+                    yCenter = 275;
+                    xCenter = 525;
+                    break;
+                }
+                case 6: {
+                    yCenter = 525;
+                    xCenter = 525;
+                    break;
+                }
+            }
+            break;
+        }
+    }
+    if (numberOfTurns >= 18 && (isActiveRed || isActiveGreen)) {
+
+        if ((((X == lastX) && (Y == lastY)))) {
+            turnOffActive(lastCenterX, lastCenterY);
         }
 
-        console.log("Index : " + X + "  " + Y + "\n");
-        console.log("Center : " + xCenter + "  " + yCenter + "\n");
+        if ((positionArray[X][Y] == 0)) {
+
+            if (((X == lastX) || (Y == lastY))) {
+                console.log("Index : " + X + "  " + Y + "\n");
+                console.log("Last Index : " + lastX + "  " + lastY + "\n");
+
+
+                if (X == 0 || X == 6 || Y == 0 || Y == 6) {
+                    if (((Math.abs(X - lastX) + Math.abs(Y - lastY)) == 3 ) || ((Math.abs(X - lastX) + Math.abs(Y - lastY)) == 1 )) {
+                        //Remove previous block and make a new block at the the given position
+                        positionArray[lastX][lastY] = 0;
+                        clearBlock(lastCenterX, lastCenterY);
+                        drawBlock(xCenter, yCenter, X, Y);
+                    }
+                } else if (X == 1 || X == 5 || Y == 1 || Y == 5) {
+                    if (((Math.abs(X - lastX) + Math.abs(Y - lastY)) == 2 ) || ((Math.abs(X - lastX) + Math.abs(Y - lastY)) == 1 )) {
+                        //Remove previous block and make a new block at the the given position
+                        positionArray[lastX][lastY] = 0;
+                        clearBlock(lastCenterX, lastCenterY);
+                        drawBlock(xCenter, yCenter, X, Y);
+                    }
+                } else if (X == 2 || X == 4 || Y == 2 || Y == 4) {
+                    if (((Math.abs(X - lastX) + Math.abs(Y - lastY)) == 1 )) {
+                        //Remove previous block and make a new block at the the given position
+                        positionArray[lastX][lastY] = 0;
+                        clearBlock(lastCenterX, lastCenterY);
+                        drawBlock(xCenter, yCenter, X, Y);
+                    }
+                }
+
+            } else {
+                //Make active off
+                turnOffActive(lastCenterX, lastCenterY);
+            }
+        }
+    }
+    else if (positionArray[X][Y] == 0 && numberOfTurns < 18) {
+
+        // console.log("Index : " + X + "  " + Y + "\n");
+        // console.log("Center : " + xCenter + "  " + yCenter + "\n");
         //Place is empty, hence make a move here
         if (numberOfTurns % 2 != 0) {
-            //Player two made a move, hence made a block blue.
+            //Player two made a move, hence made a block red.
             positionArray[X][Y] = 2;
             context.beginPath();
-            context.arc(xCenter, yCenter, 16, 0, 2 * Math.PI, false);
-            context.fillStyle = 'red';
+            context.arc(xCenter, yCenter, blockWidth, 0, 2 * Math.PI, false);
+            context.fillStyle = '#F44336';
             context.fill();
-            context.lineWidth = 2;
+            context.lineWidth = strokeWidth;
             context.strokeStyle = '#003300';
             context.stroke();
-
-            document.getElementById("turn").innerHTML = "P1 turn";
+            checkMill(X, Y, 2);
+            document.getElementById("turn").innerHTML = "Turn : P1";
         }
         else {
-            //Player one just made a move, hence made a block red
+            //Player one just made a move, hence made a block green
             positionArray[X][Y] = 1;
             context.beginPath();
-            context.arc(xCenter, yCenter, 16, 0, 2 * Math.PI, false);
-            context.fillStyle = 'green';
+            context.arc(xCenter, yCenter, blockWidth, 0, 2 * Math.PI, false);
+            context.fillStyle = '#2E7D32';
             context.fill();
-            context.lineWidth = 2;
+            context.lineWidth = strokeWidth;
             context.strokeStyle = '#003300';
             context.stroke();
-
-
-            document.getElementById("turn").innerHTML = "P2 turn";
+            checkMill(X, Y, 1);
+            document.getElementById("turn").innerHTML = "Turn : P2";
 
         }
         numberOfTurns++;
-    }else if(numberOfTurns >18 && positionArray[X][Y] !=0){
-        //Do nothing when clicked on empty element and show the all possible moves that a player have after clicking on a
-        //particular position of his own color.
-        
-
     }
 
-    update();
+    else if (numberOfTurns >= 18 && positionArray[X][Y] != 0) {
+        //Do nothing when clicked on empty element and show the all possible moves that
+        // a player have after clicking on a  particular position of his own color.
+
+        if (numberOfTurns % 2 != 0 && positionArray[X][Y] == 2) {
+            //Player two made a move, hence made a block fade red.
+            isActiveRed = true;
+            updateLastParam(xCenter, yCenter, X, Y);
+            context.beginPath();
+            context.arc(xCenter, yCenter, blockWidth, 0, 2 * Math.PI, false);
+            context.fillStyle = '#FFCDD2';
+            context.fill();
+            context.lineWidth = strokeWidth;
+            context.strokeStyle = '#003300';
+            context.stroke();
+        }
+        else if (numberOfTurns % 2 == 0 && positionArray[X][Y] == 1) {
+            //Player one just made a move, hence made a block green
+            isActiveGreen = true;
+            updateLastParam(xCenter, yCenter, X, Y);
+            context.beginPath();
+            context.arc(xCenter, yCenter, blockWidth, 0, 2 * Math.PI, false);
+            context.fillStyle = '#AED581';
+            context.fill();
+            context.lineWidth = strokeWidth;
+            context.strokeStyle = '#003300';
+            context.stroke();
+        }
+
+    }
 }
 
 canvas.addEventListener("click", mouseClick);
@@ -310,5 +380,78 @@ function mouseClick(event) {
         } else if ((X >= 475 && X <= 550) && (Y >= 475 && Y <= 550)) {
             makeMove(6, 6);
         }
+    }
+}
+
+function updateLastParam(xCenter, yCenter, X, Y) {
+    lastCenterX = xCenter;
+    lastCenterY = yCenter;
+    lastX = X;
+    lastY = Y;
+}
+
+function turnOffActive(x, y) {
+    context.beginPath();
+    context.arc(x, y, blockWidth, 0, 2 * Math.PI, false);
+    if (isActiveRed) {
+        context.fillStyle = '#F44336';
+    } else {
+        context.fillStyle = '#2E7D32';
+    }
+    context.fill();
+    context.lineWidth = 2;
+    context.strokeStyle = '#003300';
+    context.stroke();
+    isActiveRed = false;
+    isActiveGreen = false;
+}
+
+function clearBlock(xI, yI) {
+    //Clear canvas at previous position
+    context.clearRect(xI - blockWidth - strokeWidth, yI - blockWidth - strokeWidth,
+        2 * (blockWidth + strokeWidth), 2 * ( blockWidth + strokeWidth));
+    positionArray[lastX][lastY] = 0;
+
+}
+
+function drawBlock(x, y, X, Y) {
+    context.beginPath();
+    context.arc(x, y, blockWidth, 0, 2 * Math.PI, false);
+    if (isActiveRed) {
+        positionArray[X][Y] = 2;
+        context.fillStyle = '#F44336';
+        checkMill(X, Y, 2);
+    } else {
+        positionArray[X][Y] = 1;
+        context.fillStyle = '#2E7D32';
+        checkMill(X, Y, 1);
+    }
+    context.fill();
+    context.lineWidth = 2;
+    context.strokeStyle = '#003300';
+    context.stroke();
+
+    isActiveGreen = false;
+    isActiveRed = false;
+    numberOfTurns++;
+    update();
+}
+function checkThreeleft() {
+    //Check if less than or equal to three players left.
+
+}
+function checkMill(x, y, playerCode) {
+    //Check if any mill is forming around the given coordinates
+
+}
+function checkGameOver() {
+    //Either less than three left or no legal move
+}
+function update() {
+    //Update player turn
+    if (numberOfTurns % 2 != 0) {
+        document.getElementById("turn").innerHTML = "Turn : P2";
+    } else {
+        document.getElementById("turn").innerHTML = "Turn : P1";
     }
 }
